aa363acaf899598f6472868826455075e402f2fb
friedballerina <aleksandra.ikonnikova@clevon.com>
2023-09-28 20:06:23 +0300

    the right solution

--- Assignment3/assignment3.cpp
+++ Assignment3/assignment3.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <vector>
 #include <array>
+#include <iostream>
 
 // Tehted muutujatega (+, -, *, /, %, ++, --, +=, -=, *=, /=, %=)
 bool RG::Homework1::assignment3()
@@ -16,29 +17,40 @@ bool RG::Homework1::assignment3()
     std::array a{2, 3, 7, 5, 6};
 
     // Suurenda muutuja i1 väärtust 1 võrra kolmel erineval viisil
-
-
+    i1++;
+    i1 = i1 + 1;
+    i1 += 1;
 
     // Eemalda muutuja i2 väärtusest i1 väärtus
-    
+    i2 += i1;
+
     // Liida muutuja d väärtusele 0.3l ja jagada seejärel i1 väärtusega
+    d = (d + 0.3l) / i1;
 
     // Liida c väärtusele i1 väärtus
+    c += i1;
 
     // Liida s väärtusele c väärtus
+    s += c;
 
     // Korruta (*=) muutuja v.at(1) väärtus v.at(2) väärtusega, mis on suurendatud ++-ga
+   
+    // std::cout << "v at 0 is " << v.at(0) << std::endl;
+    v.at(1) *= ++v.at(2);
 
-    // Liida muutujale a.at(2) väärtus v.at(3) väärtusega, siis korruta i1 väärtusega, 
+    // Liida muutujale a.at(2) väärtus v.at(3) väärtusega, siis korruta i1 väärtusega,
     // seejärel lahuta i2 väärtusega, seejärel jaga v.at(4) väärtusega
+    a.at(2) += v.at(3);
+    a.at(2) *= i1;
+    a.at(2) -= i2;
+    a.at(2) /= v.at(4);
 
     // Kontroll, et muutujad on õiget tüüpi ja väärtusega
-    return (i1 == 3)
-    && (i2 == 8ll)
-    && (d == 4.8l/3)
-    && (c == u8'g')
-    && (s == u8"Hello World!g"s)
-    && (v == (std::vector{1, 8, 4, 4, 5}))
-    && (a == (std::array{2, 3, 5, 5, 6}));
+    return (i1 == 3) && (i2 == 8ll)
+        && (d == 4.8l/3)
+        && (c == u8'g')
+        && (s == u8"Hello World!g"s)
+        && (a == (std::array{2, 3, 5, 5, 6}))
+        && (v == (std::vector{1, 8, 4, 4, 5})) // something with v... 
+        ;
 }
-
